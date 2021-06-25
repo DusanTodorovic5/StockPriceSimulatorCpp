d26e80c5756aa86d015007bbf61f530f8bf03474
Dušan Todorović <dtodorovic40@gmail.com>
2021-06-25 19:34:14 +0200

    removed build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/disk3/POOP Projekat/Projekat
-# Build directory: /mnt/disk3/POOP Projekat/Projekat/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
